b0e0f8e8393ad9e5b61ec0b18cdd5ed2795d6afe
dhannanjai <dhannanjainautiyal@gmail.com>
2017-07-23 19:36:04 +0530

    Added Vec2 Class in int denomination(Vec2i).

---
+++ Engine/Vec2i.cpp
@@ -0,0 +1,56 @@
+#include "Vec2i.h"
+
+Vec2i::Vec2i(int x, int y)
+{
+	this->x = x;
+	this->y = y;
+}
+
+Vec2i Vec2i::operator+(const Vec2i & rhs) const
+{
+	return Vec2i(x + rhs.x, y + rhs.y);
+}
+
+Vec2i & Vec2i::operator+=(const Vec2i & rhs)
+{
+	*this = *this + rhs;
+	return *this;
+	// TODO: insert return statement here
+}
+
+Vec2i Vec2i::operator-(const Vec2i & rhs) const
+{
+	return Vec2i(x - rhs.x, y - rhs.y);
+}
+
+Vec2i & Vec2i::operator-=(const Vec2i & rhs)
+{
+	*this = *this - rhs;
+	return *this;
+	// TODO: insert return statement here
+}
+
+Vec2i Vec2i::operator*(int rhs) const
+{
+	return Vec2i(x*rhs, y*rhs);
+}
+
+Vec2i & Vec2i::operator*=(int rhs)
+{
+	*this = *this * rhs;
+	return *this;
+	// TODO: insert return statement here
+}
+
+Vec2i Vec2i::operator/(int rhs) const
+{
+	return Vec2i(static_cast<int>(x / rhs), static_cast<int>(y / rhs));
+}
+
+Vec2i & Vec2i::operator/=(int rhs)
+{
+	*this = *this / rhs;
+	return *this;
+	// TODO: insert return statement here
+}
+
